3f772e1dec84fb13086d259f6b3f891c349d51cf
Abdul Shaikh <abdulshaikhr99@gmail.com>
2025-08-27 23:03:48 -0400

    commited all files

--- main.tf
+++ main.tf
@@ -73,6 +73,7 @@ data "aws_iam_policy_document" "public_read" {
 resource "aws_s3_bucket_policy" "public" {
   bucket = aws_s3_bucket.site.id
   policy = data.aws_iam_policy_document.public_read.json
+  depends_on = [aws_s3_bucket_public_access_block.this]
 }
 
 # helpful outputs
